bbf0b8d75c98b82cc48d335a387789e267d968fc
Pablo Leal <pabloleal7@hotmail.com>
2021-11-05 23:49:39 -0300

    adicionado table

--- main.js
+++ main.js
@@ -2,13 +2,28 @@ const botaoCadastrar = document.querySelector('.botao')
 
 const elTarefa = document.querySelector('#tarefa')
 const elData = document.querySelector('#data')
+const table = document.querySelector('.table')
 
 
 let statuss = "pendente"
 
 botaoCadastrar.addEventListener('click', ()=>{
-localStorage.setItem("tarefa",elTarefa.value)
+/* localStorage.setItem("tarefa",elTarefa.value)
 localStorage.setItem("data",elData.value)
 localStorage.setItem("status",statuss)
-console.log("teste")
+console.log("teste") */
+const tr = document.createElement("tr")
+let tdTarefa  = document.createElement("td")
+tdTarefa.innerText =  elTarefa.value
+tr.append(tdTarefa)
+let tdData  = document.createElement("td")
+tdData.innerText = elData.value
+tr.append(tdData)
+let tdStatus  = document.createElement("td")
+tdStatus.innerText = "Pendente"
+tr.append(tdStatus)
+table.append(tr)
+
+console.log(elData.value)
+
 })
