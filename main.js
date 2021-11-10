3c91e688b1d876ca0c027d48c0921aa0c0d2f81c
Pablo Leal <pabloleal7@hotmail.com>
2021-11-10 14:47:13 -0300

    melhoria no cogido e localStorage Funcionando

--- main.js
+++ main.js
@@ -1,39 +1,89 @@
 const botaoCadastrar = document.querySelector('.botao')
-
 const elTarefa = document.querySelector('#tarefa')
 const elData = document.querySelector('#data')
-const table = document.querySelector('.table')
+const table = document.querySelector('.tbody')
+const elExcluir = document.querySelectorAll('.excluir')
+
+let tarefa = new Array
 
 
-let statuss = "pendente"
+if(localStorage.getItem("tarefa") !==  null){
+  tarefa  = JSON.parse(localStorage.getItem("tarefa"))
+  atualizaTela()
+} 
 
-botaoCadastrar.addEventListener('click', ()=>{
-/* localStorage.setItem("tarefa",elTarefa.value)
-localStorage.setItem("data",elData.value)
-localStorage.setItem("status",statuss)
-console.log("teste") */
-const tr = document.createElement("tr")
-let tdTarefa  = document.createElement("td")
-tdTarefa.innerText =  elTarefa.value
-tr.append(tdTarefa)
-let tdData  = document.createElement("td")
-tdData.innerText = elData.value
-tr.append(tdData)
-let tdStatus  = document.createElement("td")
-tdStatus.innerText = "Pendente"
-tr.append(tdStatus)
+function adicionaTarefa() {
 
-let tdExcluir  = document.createElement("td")
-tdExcluir.innerHTML = "<button>Excluir</button>"
-tr.append(tdExcluir)
+  if (elTarefa.value && elData.value !== "") {
+    tarefa.push({ nome: elTarefa.value, data: elData.value, id: tarefa.length })
+    atualizaTela()
+  
+    atualizaStorage()
+  } else {
+    alert("Preencha todos os campos!")
+  }
 
-if((elTarefa.value && elData.value) != ""){
-  table.append(tr)
-}else{
-  alert("Preencha todos os Campos")
+ 
+
+}
+
+function atualizaStorage(){
+  localStorage.clear("tarefa")
+  if(tarefa.length !== 0 ){
+    localStorage.setItem("tarefa", JSON.stringify(tarefa))
+  }
+  
+}
+
+function excluir(id) {
+tarefa.splice(id,1)
+atualizaStorage()
+atualizaTela()
+}
+
+function atualizaTela() {
+  table.innerHTML = ""
+
+  if(tarefa.length === 0 ){
+    return
+  }
+
+  tarefa.forEach((item,i) => {
+    const tr = document.createElement("tr")
+
+    let tdTarefa = document.createElement("td")
+    tdTarefa.innerText = item.nome
+    tr.append(tdTarefa)
+
+    let tdData = document.createElement("td")
+    tdData.innerText = item.data
+    tr.append(tdData)
+
+    let tdExcluir = document.createElement("td")
+    tdExcluir.innerHTML = `<button  onClick="excluir(${i})" class="excluir">Excluir</button>`
+
+    tr.append(tdExcluir)
+
+    table.append(tr)
+    elData.value = ""
+    elTarefa.value = ""
+
+  })
+
+  /* 
+
+ 
+
+  ) */
 }
 
 
-console.log(elData.value)
+botaoCadastrar.addEventListener('click', () => {
+
+  adicionaTarefa()
+  
+ 
 
 })
+
+
